0f5d9a1fb77e231c1c41ff11cab29f4537537457
woojin <jwjley@naver.com>
2023-05-12 23:36:23 +0900

    feat : 지원정보 상세리스트 추가 (2023-05-12 우진)

--- app/src/main/java/kr/co/company/pfsi_app/SupportInfoAdapter.java
+++ app/src/main/java/kr/co/company/pfsi_app/SupportInfoAdapter.java
@@ -1,5 +1,7 @@
 package kr.co.company.pfsi_app;
 
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,9 +33,35 @@ public class SupportInfoAdapter extends RecyclerView.Adapter<SupportInfoAdapter.
     public void onBindViewHolder(@NonNull CustomViewHolder holder, int position) {
         holder.programTitle.setText(arrayList.get(position).getProgramTitle());
         holder.time.setText(arrayList.get(position).getTime());
-        holder.phone.setText(arrayList.get(position).getTime());
+        holder.phone.setText(arrayList.get(position).getPhone());
         holder.group.setText(arrayList.get(position).getGroup());
         holder.city.setText(arrayList.get(position).getCity());
+
+        // 리사이클러뷰 클릭이벤트(선택된 리사이클러뷰 화면으로 이동)
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int mPosition = holder.getAdapterPosition();
+                Context context = view.getContext();
+
+                Intent intent = new Intent(context, SupportProgramInfoPostActivity.class);
+                intent.putExtra("city", arrayList.get(mPosition).getCity());
+                intent.putExtra("group", arrayList.get(mPosition).getGroup());
+                intent.putExtra("peopleState", arrayList.get(mPosition).getPeopleState());
+                intent.putExtra("peopleStateInfo", arrayList.get(mPosition).getPeopleStateInfo());
+                intent.putExtra("category", arrayList.get(mPosition).getCategory());
+                intent.putExtra("categoryInfo", arrayList.get(mPosition).getCategoryInfo());
+                intent.putExtra("programTitle", arrayList.get(mPosition).getProgramTitle());
+                intent.putExtra("time", arrayList.get(mPosition).getTime());
+                intent.putExtra("money", arrayList.get(mPosition).getMoney());
+                intent.putExtra("address", arrayList.get(mPosition).getAddress());
+                intent.putExtra("phone", arrayList.get(mPosition).getPhone());
+                intent.putExtra("programContent", arrayList.get(mPosition).getProgramContent());
+
+                (context).startActivity(intent);
+
+            }
+        });
     }
 
     @Override
--- app/src/main/java/kr/co/company/pfsi_app/SupportProgramInfoPostActivity.java
+++ app/src/main/java/kr/co/company/pfsi_app/SupportProgramInfoPostActivity.java
@@ -2,13 +2,63 @@ package kr.co.company.pfsi_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SupportProgramInfoPostActivity extends AppCompatActivity {
+
+    private TextView TVcity, TVgroup, TVpeopleState, TVpeopleStateInfo, TVcategory, TVcategoryInfo, TVprogramTitle, TVtime, TVmoney, TVaddress, TVphone, TVprogramContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support_program_info_post);
+
+        TVcity = (TextView) findViewById(R.id.city);
+        TVgroup = (TextView) findViewById(R.id.group);
+        TVpeopleState = (TextView) findViewById(R.id.peopleState);
+        TVpeopleStateInfo = (TextView) findViewById(R.id.peopleStateInfo);
+        TVcategory = (TextView) findViewById(R.id.category);
+        TVcategoryInfo = (TextView) findViewById(R.id.categoryInfo);
+        TVprogramTitle = (TextView) findViewById(R.id.programTitle);
+        TVtime = (TextView) findViewById(R.id.time);
+        TVmoney = (TextView) findViewById(R.id.money);
+        TVaddress = (TextView) findViewById(R.id.address);
+        TVphone = (TextView) findViewById(R.id.phone);
+        TVprogramContent = (TextView) findViewById(R.id.programContent);
+
+
+        Intent receiveIntent = getIntent();
+        final String city = receiveIntent.getStringExtra("city");
+        final String group = receiveIntent.getStringExtra("group");
+        final String peopleState = receiveIntent.getStringExtra("peopleState");
+        final String peopleStateInfo = receiveIntent.getStringExtra("peopleStateInfo");
+        final String category = receiveIntent.getStringExtra("category");
+        final String categoryInfo = receiveIntent.getStringExtra("categoryInfo");
+        final String programTitle = receiveIntent.getStringExtra("programTitle");
+        final String time = receiveIntent.getStringExtra("time");
+        final String money = receiveIntent.getStringExtra("money");
+        final String address = receiveIntent.getStringExtra("address");
+        final String phone = receiveIntent.getStringExtra("phone");
+        final String programContent = receiveIntent.getStringExtra("programContent");
+
+        TVcity.setText(city);
+        TVgroup.setText(group);
+        TVpeopleState.setText(peopleState);
+        TVpeopleStateInfo.setText(peopleStateInfo);
+        TVcategory.setText(category);
+        TVcategoryInfo.setText(categoryInfo);
+        TVprogramTitle.setText(programTitle);
+        TVtime.setText(time);
+        TVmoney.setText(money);
+        TVaddress.setText(address);
+        TVphone.setText(phone);
+        TVprogramContent.setText(programContent);
+
     }
 }
