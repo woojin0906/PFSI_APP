697241edd1107fe180fcdc7cff0bf85baef3abd2
Lib0823 <robot082373@naver.com>
2023-05-16 18:59:02 +0900

    feat: WelfareMapActivity TMap api 연동 (2023-05-16 인범)

--- app/src/main/java/kr/co/company/pfsi_app/WelfareMapActivity.java
+++ app/src/main/java/kr/co/company/pfsi_app/WelfareMapActivity.java
@@ -3,12 +3,24 @@ package kr.co.company.pfsi_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.skt.Tmap.TMapView;
 
 public class WelfareMapActivity extends AppCompatActivity {
+
+    private String api_key = "WDUOyZdacml2BjQXwZJL2PDCzymCNXM4U6HOF5td";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welfare_map);
+
+        LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
+        TMapView tMapView = new TMapView(this);
+
+        tMapView.setSKTMapApiKey( api_key );
+        linearLayoutTmap.addView( tMapView );
+
     }
 }
